6cb18d0cac0b1a7c9ac831d06f2decb90c8b1388
sachin <sachin.rajput@analyticsvidhya.com>
2026-09-08 22:29:26 +0530

    tables

---
+++ models.py
@@ -0,0 +1,65 @@
+import enum
+from decimal import Decimal
+
+from datetime import datetime
+from typing import List, Optional
+
+from sqlalchemy import String, Numeric, DateTime, func, ForeignKey, Enum
+from sqlalchemy.orm import DeclarativeBase, Mapped, relationship, mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+class TimeStamp(DeclarativeBase):
+    __tablename__ = "timestamp"
+    created_at : Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at : Mapped[datetime] = mapped_column(DateTime(timezone=True),server_default=func.now(), onupdate=func.now())
+
+class OrderType(str, enum.Enum):
+    BUY="BUY"
+    SELL="SELL"
+
+class OrderExecutionType(str, enum.Enum):
+    MARKET = "MARKET"
+    LIMIT = "LIMIT"
+
+class OrderStatus(str, enum.Enum):
+    PENDING="PENDING"
+    COMPLETED="COMPLETED"
+    CANCELLED="CANCELLED"
+
+class User(DeclarativeBase, TimeStamp):
+    id : Mapped[int] = mapped_column(primary_key=True)
+    username : Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email : Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    password : Mapped[str] = mapped_column(String(255), nullable=False) #hashed
+
+    available_funds : Mapped[int] = mapped_column(Numeric(14,2), default=Decimal(10000000), nullable=False)
+    holdings : Mapped[List["Holding"]] = relationship("Holding", back_populates="user", cascade="all, delete-orphan")
+    orders : Mapped[List["Order"]] = relationship("Order", back_populates="user", cascade="all, delete-orphan")
+
+class Holding(DeclarativeBase, TimeStamp):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), ondelete="CASCADE")
+    symbol : Mapped[str] = mapped_column(String(15), nullable=False)
+
+    quantity : Mapped[Decimal] = mapped_column(Numeric(precision=10, scale=2), nullable=False)
+    average_price : Mapped[Decimal] = mapped_column(Numeric(precision=10, scale=2), nullable=False)
+
+    user : Mapped[User] = relationship("User", back_populates="holdings")
+
+class Order(DeclarativeBase, TimeStamp):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id : Mapped[int] = mapped_column(ForeignKey("user.id"), ondelete="CASCADE")
+    symbol : Mapped[str] = mapped_column(String(15), nullable=False)
+    execution_type : Mapped[str] = mapped_column(Enum(OrderExecutionType), nullable=False)
+    order_type : Mapped[str] = mapped_column(Enum(OrderType), nullable=False)
+    status : Mapped[str] = mapped_column(Enum(OrderStatus), nullable=False)
+
+    quantity : Mapped[Decimal] = mapped_column(Numeric(precision=10, scale=2), nullable=False)
+    execution_price: Mapped[Optional[Decimal]] = mapped_column(Numeric(precision=14, scale=2), nullable=True)
+    limit_price: Mapped[Optional[Decimal]] = mapped_column(Numeric(precision=14, scale=2), nullable=True)
+    total_amount: Mapped[Optional[Decimal]] = mapped_column(Numeric(precision=14, scale=2), nullable=True)
+
+    completed_at : Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    user : Mapped[User] = relationship("User", back_populates="orders")
